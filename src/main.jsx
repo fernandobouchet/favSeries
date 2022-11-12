be2b637c3cdd399360109db659670effca70524f
fernandobouchet <fernandobouchet@gmail.com>
2022-11-12 17:52:54 -0300

    Agregadas dependencias de 'react-router' y configuración inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
