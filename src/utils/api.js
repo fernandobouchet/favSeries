ddb3cf4d6681658a41af0c019ba0a99fc62a7eab
fernandobouchet <fernandobouchet@gmail.com>
2022-11-16 20:20:35 -0300

    Cambios en la especificaciones de los datos que debe traer la API

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const getSeries = async (page) => {
     const data = await axios.get(
       `https://api.themoviedb.org/3/discover/tv?api_key=${
         import.meta.env.VITE_APP_API_KEY
-      }&language=es-MEX&sort_by=popularity.desc&vote_count.gte=1400&watch_region=AR&page=1&with_watch_providers=8|119|384&without_genres=10762,16&first_air_date.gte=2000-01-01&append_to_response=credits`,
+      }&language=es-MEX&sort_by=vote_count.desc&vote_count.gte=1400&watch_region=AR&page=1&with_watch_providers=&without_genres=10762,16&first_air_date.gte=2000-01-01`,
       {
         params: {
           page: page,
